1cb3ef21d5899fdad80964d49c75405c4123d2bb
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-09-03 13:31:01 +0200

    remove unused classes add models

---
+++ BookWyrmBackend/Models/BookHoard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookWyrmBackend.Models
+{
+    public class BookHoard
+    {
+        public BookHoard()
+        {
+            Hoard = new List<Book>();
+        }
+
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public List<Book> Hoard { get; set; }
+
+    }
+}
---
+++ BookWyrmBackend/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookWyrmBackend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public List<BookHoard> BookHoards { get; set; }
+        public string AuthToken { get; set; }
+
+        internal string GenerateAuthToken()
+        {
+            string toHash = Username + Password + Stopwatch.GetTimestamp().ToString();
+            return toHash.GetHashCode().ToString();
+        }
+    }
+}
