ef730cf5628944351b393a9e9dd4f1ff5fcff579
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-09-09 16:13:19 +0200

    add usercontext

---
+++ BookWyrmBackend/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using BookWyrmBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWyrmBackend
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<BookWyrmBackend.Models.BookHoard> BookHoards { get; set; }
+
+        public DbSet<BookWyrmBackend.Models.Book> Books { get; set; }
+    }
+}
