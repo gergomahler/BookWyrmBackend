28048ba0a21be34c5c0e55c2c2daa269ab160316
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-09-15 11:02:37 +0200

    add book props, create author model

--- BookWyrmBackend/Models/Book.cs
+++ BookWyrmBackend/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace BookWyrmBackend.Models
@@ -9,6 +10,11 @@ namespace BookWyrmBackend.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Author { get; set; }
+        public string Description { get; set; }
+        public List<Author> Authors { get; set; }
+        public string ISBN { get; set; }
+        public string Language { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishDate { get; set; }
     }
 }
